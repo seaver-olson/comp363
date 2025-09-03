e1cf217245bec7b68d79337991b59443402654b5
Seaver Olson <46256937+seaver-olson@users.noreply.github.com>
2025-09-03 13:32:59 -0500

    Add Heap struct and main function in cImp.c

---
+++ week0/cImp.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef struct{
+    int cap;
+    int size;
+    int *array;
+} Heap;
+
+int main(){
+    int example[] = {999999,23,34,64,12,22,11,1,2,3,56,23,75};
+    int exampleSize = sizeof(example)/sizeof(example[0]);
+    printf("===ADD TEST===\n");
+    
+    return 0;
+}
