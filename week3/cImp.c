063fb44a93d11048e24468817831e21187e46af8
Seaver Olson <seaver@Seavers-MacBook-Air.local>
2025-09-26 15:29:16 -0500

    added a lot of work to week3

--- week3/cImp.c
+++ week3/cImp.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #define ALPHABET_COUNT 26
+#define TABLE_COUNT 27
 
-char *input_string = "Hello";
+char input_string[] = "HELLO WORLD";
 int size = (sizeof(input_string)/sizeof(input_string[0]));
-int freqs[ALPHABET_COUNT];
+int freqs[TABLE_COUNT];
 
-struct Node{
+typedef struct Node{
     int freq;
     char sym;
     struct Node *left;
     struct Node *right;
-};
+}Node;
+
+Node* create_node(int frequency, char symbol) {
+    Node* node = (Node*)malloc(sizeof(Node));
+    node->freq = frequency;
+    node->sym = symbol;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
 
 void set_left(struct Node *parent,struct Node *leftChild){
     parent->left = leftChild;
@@ -26,16 +38,43 @@ int lt(struct Node *node, struct Node *other){
     return node->freq < other->freq;
 }
 
-void count_frequencies(int freqs[ALPHABET_COUNT+1]){
+int find_min(Node* forest[], int size){
+    int min = 0;
+    for (int i=1;i<size;i++)if (lt(forest[i], forest[min])) min = i;
+    return min;
+}
+
+void filter(char* input, char* output){
+    int counter = 0;
     for (int i = 0; i < size; i++){
-        if (((int)input_string[i])-65 != 32) freqs[(int)input_string[i]-65]++;
+        if (isalpha(input[i])) output[counter++] = toupper(input[i]); 
+        else if (input[i] == ' ') output[counter++] = ' ';
+    }
+    output[counter] = '\0';//terminate bit for end of string
+}
+
+void count_frequencies(const char* input, int freqs[ALPHABET_COUNT+1]){
+    for (int i = 0; i < ALPHABET_COUNT+1; i++) {
+        freqs[i] = 0;
+    }
+
+    for (int i = 0; input[i] != '\0'; i++) {
+        char c = input[i];
+        if (c >= 'A' && c <= 'Z') {
+            freqs[c - 'A']++;
+        } else if (c == ' ') {
+            freqs[ALPHABET_COUNT]++; 
+        }
     }
 }
 
 int main(){
-    count_frequencies(freqs);
-    for (int i = 0; i < 26; i++){
-        printf("%d \n", freqs[i]);
+    char filtered[size];
+    filter(input_string, filtered);
+    count_frequencies(filtered,freqs);
+    for (int i = 0; i < ALPHABET_COUNT; i++){
+        if (freqs[i] > 0) printf("%c: %d\n", 'A' + i, freqs[i]);
     }
+    if (freqs[ALPHABET_COUNT] > 0) printf("SPACE: %d\n", freqs[ALPHABET_COUNT]);
     return 0;
 }
