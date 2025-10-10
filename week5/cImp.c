e5fc73d5ab5822024775c49a61a3c3cb672ad85e
Seaver Olson <sjolson007@gmail.com>
2025-10-10 17:04:32 -0500

    done first part

--- week5/cImp.c
+++ week5/cImp.c
@@ -18,7 +18,7 @@ typedef struct {
 } result;
 
 //Leo said to not include and nonstandard library so no strlen() :(
-int len(char* string){
+int len(const char* string){
     int len = 0;
     while (string[len] != '\0') len++;
     return len;
@@ -30,6 +30,43 @@ result align(const char* M, const char* N){
     int n = len(N);
 
     int P[m+1][n+1];//penalty matrix
+
+    int max = (m > n) ? m : n;
+    //fill in base case 
+    for (int i = 0; i <= max; i++){
+        //if i understand pipeline dynamic predictive branching like I think I do these branches should be predicted pretty early into the loop making the conditionals meaningless for performance eventually
+        if (i <= m) P[i][0] = i * GAP_PEN;
+        if (i <= n) P[0][i] = i * GAP_PEN;
+    }
+
+    for (int i=1; i <= m; i++){
+        for (int j = 1; j<=n; j++){
+            //three cases for optimal substructure
+            int diag = P[i-1][j-1] + GET_PENALTY(M[i-1],N[j-1]);
+            int up = P[i-1][j] + GAP_PEN;
+            int left = P[i][j-1] + GAP_PEN;
+
+            //get minimum penalty
+            int min = diag;
+            if (up < min) min = up;
+            if (left < min) min = left;
+            
+            //assign min
+            P[i][j] = min;
+        }
+    }
+
+    char residual1[max*2+1];
+    char residual2[max*2+1];
+
+    int r1 = m;//holds position in first residual list
+    int r2 = n;//holds position in second residual list
+
+    while (r1 > 0 && r2 > 0){
+        int curr = P[r1][r2];
+    }
+    
+    
 }
 
 int main(){
@@ -44,7 +81,8 @@ int main(){
     };
 
     for (int i = 0; i < TESTCASES; i++){
-
+        result res = align(tests[i][0], tests[i][1]);
+        printf("Testcase %d: gaps: %d, match: %d, mismatch: %d\n", i, res.gaps, res.matches, res.mismatches);
     }
     return 0;
 }
