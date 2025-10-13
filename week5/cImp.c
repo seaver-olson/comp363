3a1fd4c8f243b0af1aca2f92352353cef8322cfd
Seaver Olson <sjolson007@gmail.com>
2025-10-13 10:09:49 -0500

    could not figure it out, I am still getting really weird results, I think my sys just adds gaps if it can to nything its kind of terrible

--- week5/cImp.c
+++ week5/cImp.c
@@ -23,51 +23,84 @@ int len(const char* string){
     while (string[len] != '\0') len++;
     return len;
 }
-
 result align(const char* M, const char* N){
     result res = {0};
     int m = len(M);
     int n = len(N);
+    int P[m+1][n+1];
 
-    int P[m+1][n+1];//penalty matrix
+    for (int i = 0; i <= m; i++) P[i][0] = i * GAP_PEN;
+    for (int j = 0; j <= n; j++) P[0][j] = j * GAP_PEN;
 
-    int max = (m > n) ? m : n;
-    //fill in base case 
-    for (int i = 0; i <= max; i++){
-        //if i understand pipeline dynamic predictive branching like I think I do these branches should be predicted pretty early into the loop making the conditionals meaningless for performance eventually
-        if (i <= m) P[i][0] = i * GAP_PEN;
-        if (i <= n) P[0][i] = i * GAP_PEN;
-    }
-
-    for (int i=1; i <= m; i++){
-        for (int j = 1; j<=n; j++){
-            //three cases for optimal substructure
-            int diag = P[i-1][j-1] + GET_PENALTY(M[i-1],N[j-1]);
-            int up = P[i-1][j] + GAP_PEN;
+    for (int i = 1; i <= m; i++){
+        for (int j = 1; j <= n; j++){
+            int diag = P[i-1][j-1] + GET_PENALTY(M[i-1], N[j-1]);
+            int up   = P[i-1][j] + GAP_PEN;
             int left = P[i][j-1] + GAP_PEN;
 
-            //get minimum penalty
             int min = diag;
             if (up < min) min = up;
             if (left < min) min = left;
-            
-            //assign min
             P[i][j] = min;
         }
     }
 
-    char residual1[max*2+1];
-    char residual2[max*2+1];
-
-    int r1 = m;//holds position in first residual list
-    int r2 = n;//holds position in second residual list
+    int r1 = m, r2 = n;
+    char strM[m+n+1], strN[m+n+1];
+    int pos = 0;
 
     while (r1 > 0 && r2 > 0){
         int curr = P[r1][r2];
+        int diagCost = P[r1-1][r2-1] + GET_PENALTY(M[r1-1], N[r2-1]);
+        int upCost   = P[r1-1][r2] + GAP_PEN;
+        int leftCost = P[r1][r2-1] + GAP_PEN;
+
+        if (curr == diagCost) {
+            strM[pos] = M[r1-1];
+            strN[pos] = N[r2-1];
+            if (M[r1-1] == N[r2-1]) res.matches++;
+            else res.mismatches++;
+            r1--; r2--;
+        } else if (curr == upCost) {
+            strM[pos] = M[r1-1];
+            strN[pos] = '-';
+            res.gaps++;
+            r1--;
+        } else {
+            strM[pos] = '-';
+            strN[pos] = N[r2-1];
+            res.gaps++;
+            r2--;
+        }
+        pos++;
     }
-    
-    
+
+    while (r1 > 0){
+        strM[pos] = M[r1-1];
+        strN[pos] = '-';
+        res.gaps++; r1--; pos++;
+    }
+    while (r2 > 0){
+        strM[pos] = '-';
+        strN[pos] = N[r2-1];
+        res.gaps++; r2--; pos++;
+    }
+
+    // reverse the strings for correct display
+    for (int i = 0; i < pos / 2; i++){
+        char t = strM[i]; strM[i] = strM[pos-1-i]; strM[pos-1-i] = t;
+        t = strN[i]; strN[i] = strN[pos-1-i]; strN[pos-1-i] = t;
+    }
+
+    strM[pos] = '\0';
+    strN[pos] = '\0';
+
+    printf("%s : %s\n", strM, M);
+    printf("%s : %s\n", strN, N);
+
+    return res;
 }
+
 
 int main(){
     char* tests[TESTCASES][2] = {
