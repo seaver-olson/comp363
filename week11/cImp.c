00c5254bc43d3059aaf6d0fe31221d10c8acaa1c
Seaver Olson <seaver@Seavers-MacBook-Air.local>
2025-11-24 15:56:00 -0600

    added some work

--- week11/cImp.c
+++ week11/cImp.c
@@ -45,14 +45,50 @@ void generate_random_string(char* string, int length, float distortion_prob){
     string[write_pointer] = '\0';
 }
 
+void can_segment_dp(const char* string, char* outSegs[NUM_OF_WORDS]){
+    int dp[NUM_OF_WORDS+1] = {0};
+    dp[0]=1;
+    for (int i = 1; i <= NUM_OF_TRIALS; i++){
+        int j = 0;
+        while (j < i && !dp[i]){
+            if (!dp[j]) {
+                j++;
+                continue;
+            }
+            int len = i - j;
+            char* slice = malloc(len + 1);
+            memcpy(slice, string + j, len);
+            slice[len] = '\0';
+            if (is_word(slice)) {
+                dp[i] = 1;
+                outSegs[i - 1] = slice;
+                break;
+            }
+            j++;
+            free(slice);
+        }
+    }
+}
+
 int main(){
     srand(time(0));
     while (current_trial < NUM_OF_TRIALS){
         current_trial++;
+        char* outSegs[NUM_OF_WORDS];
         char* trial_string = (char*)malloc(sizeof(char) * NUM_OF_WORDS * 20);//okay i didnt really know what to do here so i just assumed max word length is 20 for malloc
         generate_random_string(trial_string, NUM_OF_WORDS, DISTORTION_PROB);
         printf("Trial %d: Testing string \"%s\"\n", current_trial, trial_string);
-        
+        can_segment_dp(trial_string, outSegs);
+        printf("Segmented as: ");
+        for (int i = 0; i < NUM_OF_WORDS; i++){
+            if (outSegs[i]){
+                printf("%s ", outSegs[i]);
+                free(outSegs[i]);
+            }
+        }
+        printf("There are %svalid segmentations.\n", outSegs[NUM_OF_WORDS - 1] ? "" : "no ");
+        printf("\n");
+        free(trial_string);
     }
     return 0;
 }
